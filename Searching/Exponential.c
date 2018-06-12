714394db20f181273e43fabb5697b2b860d5c8a3
Sumit Chaudhary <digitalcodecorporation@gmail.com>
2018-06-12 19:54:28 +0530

    Exponential Search

--- Searching/Exponential.c
+++ Searching/Exponential.c
@@ -1,10 +1,30 @@
 // Array needs to be sorted for this to be work.
+// Refer GeeksForGeeks for explantion.
 #include<stdio.h>
-
-int exponential(int array[],int n,int x){
-  // Write code here...
+int binarySearch(int array[],int start,int end,int x){
+  while(start<=end){
+    int pos = start+(end-start)/2;
+    if(x == array[pos]){ return pos;}
+    if(x < array[pos]){
+      end = pos-1;
+    } else{
+      start = pos + 1;
+    }
+  }
   return -1;
 }
+int exponential(int array[],int n,int x){
+  if(array[0] == x){
+    return 0;
+  }
+  int i=1;
+  while(i < n && array[i] <=x){
+    i *= 2;
+  }
+  printf("Index : %d",i);
+  return binarySearch(array,i/2,i<n?i:n,x);
+}
+
 int main(){
   int array[] = {1,2,3,4,5,6};
   int x = 5;
